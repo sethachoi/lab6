7972d1039134cf01c8e70734697897220943a72a
Seth Choi <sac061@ucsd.edu>
2016-02-11 00:02:18 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -11,7 +11,9 @@ $(document).ready(function() {
 function initializePage() {
 	$('.project a').click(addProjectDetails);
 
-	$('#colorBtn').click(randomizeColors);
+	$('#colorBtn').click(function () {
+		$.get("/palette/", randomizeColors);
+	});
 }
 
 /*
@@ -21,12 +23,17 @@ function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
 
+	var details;
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	$.get("/project/" + idNumber, callBack);
+	//console.log("/project/" + idNumber);
+
 
 	console.log("User clicked on project " + idNumber);
+
 }
 
 /*
@@ -35,4 +42,21 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	var colors = e["colors"]["hex"];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
+
+function callBack(result) {
+	console.log(result);
+	var projectHTML = '<a href="#" class="thumbnail">' +
+	'<img src="'+ result['image'] + '"class="img detailsImage">' +
+	'<p>' + result['title'] + '</p>' +
+	'<p><small>' + result['date'] + '</small></p></a>' + result['summary'];
+	$("#project" + result['id'] + " .details").html(projectHTML);
+}
+
